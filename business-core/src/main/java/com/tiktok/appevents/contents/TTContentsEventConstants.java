69199ca28954085bde506db26c8fe0ba0d90cc35
chaofeng01 <wangchaofeng.fengny@tiktok.com>
2024-05-30 10:35:11 +0800

    fix: Add missing currency

--- business-core/src/main/java/com/tiktok/appevents/contents/TTContentsEventConstants.java
+++ business-core/src/main/java/com/tiktok/appevents/contents/TTContentsEventConstants.java
@@ -29,6 +29,6 @@ public interface TTContentsEventConstants {
         AED, ARS, AUD, BDT, BHD, BIF, BOB, BRL, CAD, CHF, CLP, CNY, COP, CRC, CZK, DKK, DZD, EGP,
         EUR, GBP, GTQ, HKD, HNL, HUF, IDR, ILS, INR, ISK, JPY, KES, KHR, KRW, KWD, KZT, MAD, MOP,
         MXN, MYR, NGN, NIO, NOK, NZD, OMR, PEN, PHP, PKR, PLN, PYG, QAR, RON, RUB, SAR, SEK, SGD,
-        THB, TRY, TWD, UAH, USD, VES, VND, ZAR
+        THB, TRY, TWD, UAH, USD, VES, VND, ZAR, BGN, IQD,JOD, LBP, TZS
     }
 }
